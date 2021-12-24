11dad40cfb927df527a3b7ae8dec5413052c1a8b
kshivam1177 <shivam@ivypods.com>
2021-12-24 16:39:27 +0530

    Work in Progress

--- test/method_channel_video_player_test.dart
+++ test/method_channel_video_player_test.dart
@@ -6,9 +6,9 @@ import 'dart:ui';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:video_player_platform_interface/messages.dart';
-import 'package:video_player_platform_interface/method_channel_video_player.dart';
-import 'package:video_player_platform_interface/video_player_platform_interface.dart';
+import 'package:yc_video_player_platform_interface/messages.dart';
+import 'package:yc_video_player_platform_interface/method_channel_video_player.dart';
+import 'package:yc_video_player_platform_interface/video_player_platform_interface.dart';
 
 import 'test.dart';
 
